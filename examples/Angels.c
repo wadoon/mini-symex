69198d976d43431f83925ef40a074863a148f356
Alexander Weigl <weigl@kit.edu>
2021-06-17 14:36:50 +0200

    update example

--- examples/Angels.c
+++ examples/Angels.c
@@ -1,9 +1,5 @@
 void main() {
-  int[] arr;
-  int i = 0;
-  l1: while( i < 5) {
-    arr[i] = i;
-    i = i + 1;
-  }
-  assert (\forall int i; 0 <= i && i < 5 ==> arr[i] == i);
+  int x = 0;
+  choose x : x > 0;
+  assert x == 2;
 }
